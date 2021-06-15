47c490f0dae03182a799c465ffe73f2d98a87dc0
l3n641 <371522155@qq.com>
2021-06-15 11:40:19 +0800

    add 订单列表查看(买家视角)

--- aop/api/__init__.py
+++ aop/api/__init__.py
@@ -1,3 +1,4 @@
 from aop.api.base import FileItem, BaseApi
 from aop.api.transaction.trade_receive_address import AlibabaTradeReceiveAddressGetParam
 from aop.api.transaction.trade_buyer_order_list import AlibabaTradeGetBuyerOrderListParam
+from aop.api.transaction.trade_buyer_view import AlibabaTradeGetBuyerViewParam
